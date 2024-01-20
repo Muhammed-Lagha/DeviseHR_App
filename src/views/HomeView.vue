38d932a7b4cc952b1c101045d6b7e764be956a30
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-01-20 20:24:51 +0200

    install VueUse library and init DarkMode

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { ref } from 'vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+const isAr = ref(false)
+const toggleAr = () => (isAr.value = !isAr.value)
+const en = {
+  'log in': 'Log In'
+}
+const ar = {
+  'log in': 'تسجيل الدخول'
+}
+</script>
 
 <template>
-  <main></main>
+  <main
+    :dir="isAr ? 'rtl' : 'ltr'"
+    class="pt-8 flex items-start justify-center bg-slate-700 dark:bg-white min-h-screen"
+  >
+    <div class="max-w-[700px] shadow-md p-6 rounded-md bg-white dark:bg-slate-700">
+      <div class="flex items-start sm:items-center mb-4">
+        <h1 class="text-4xl flex-1 dark:text-white">{{ isAr ? ar['log in'] : en['log in'] }}</h1>
+        <span class="dark:text-white"> {{ isDark ? 'Dark' : 'Light' }} Mode </span>
+      </div>
+      <p class="text-md mb-4 dark:text-white">
+        In this tutorial we will be learning how to enable dark mode for your tailwind website/app.
+        Also, we will show how we can create a toggle to change between a light & dark theme.
+      </p>
+      <button
+        @click="toggleDark()"
+        class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
+      >
+        Learn More!
+      </button>
+      <button
+        @click="toggleAr()"
+        class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
+      >
+        toggle language
+      </button>
+    </div>
+  </main>
 </template>
