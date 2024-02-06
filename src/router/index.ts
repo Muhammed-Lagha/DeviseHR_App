f3ac61e841c9313068f24ef48812f94198cb27cd
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-02-06 23:15:57 +0200

    /

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
+import EmployeesView from '../views/EmployeesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,8 +14,13 @@ const router = createRouter({
     },
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
+    },
+    {
+      path: '/employees',
+      name: 'Employees',
+      component: EmployeesView
     },
   ]
 })
