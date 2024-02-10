42f0b53bd9ad4bccd9897ed1ac3f91a179e5a847
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-02-10 22:27:33 +0200

    generate letter image for employee engagement ,add banner to teams post

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,45 +1,40 @@
 <script setup lang="ts">
+import AuthenticatedLayout from '@/components/Layout/AuthenticatedLayout.vue'
 import { useDark, useToggle } from '@vueuse/core'
 import { ref } from 'vue'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+
 const isAr = ref(false)
-const toggleAr = () => (isAr.value = !isAr.value)
-const en = {
-  'log in': 'Log In'
-}
-const ar = {
-  'log in': 'تسجيل الدخول'
-}
 </script>
 
 <template>
-  <main
-    :dir="isAr ? 'rtl' : 'ltr'"
-    class="pt-8 flex items-start justify-center bg-slate-700 dark:bg-white min-h-screen"
-  >
-    <div class="max-w-[700px] shadow-md p-6 rounded-md bg-white dark:bg-slate-700">
-      <div class="flex items-start sm:items-center mb-4">
-        <h1 class="text-4xl flex-1 dark:text-white">{{ isAr ? ar['log in'] : en['log in'] }}</h1>
-        <span class="dark:text-white"> {{ isDark ? 'Dark' : 'Light' }} Mode </span>
+  <AuthenticatedLayout>
+    <main :dir="isAr == true ? 'rtl' : 'ltr'">
+      <div class="max-w-[700px] shadow-md p-6 rounded-md bg-white dark:bg-slate-700">
+        <div class="flex items-start sm:items-center mb-4">
+          <h1 class="text-4xl flex-1 dark:text-white">{{}}</h1>
+          <span class="dark:text-white"> {{ isDark ? 'Dark' : 'Light' }} Mode </span>
+        </div>
+        <p class="text-md mb-4 dark:text-white">
+          {{ isAr ? 'مرحبا بكم في DeviseHR' : 'Welcome to DeviseHR' }}
+        </p>
+        <div class="flex items-center justify-between">
+          <button
+            @click="toggleDark()"
+            class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
+          >
+            {{ isDark ? 'Light' : 'Dark' }}
+          </button>
+          <button
+            @click="isAr = !isAr"
+            class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
+          >
+            {{ isAr ? 'English' : 'Arabic' }}
+          </button>
+        </div>
       </div>
-      <p class="text-md mb-4 dark:text-white">
-        In this tutorial we will be learning how to enable dark mode for your tailwind website/app.
-        Also, we will show how we can create a toggle to change between a light & dark theme.
-      </p>
-      <button
-        @click="toggleDark()"
-        class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
-      >
-        Learn More!
-      </button>
-      <button
-        @click="toggleAr()"
-        class="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-black"
-      >
-        toggle language
-      </button>
-    </div>
-  </main>
+    </main>
+  </AuthenticatedLayout>
 </template>
