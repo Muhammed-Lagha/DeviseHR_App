3ccdc53b9e5e5ac4c22a5a2cba0832998e369ae5
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-02-13 22:39:57 +0200

    add settings router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
 import EmployeesView from '../views/EmployeesView.vue'
 import CalendarView from '../views/CalenderView.vue'
+import SettingsView from '../views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       name: 'Calendar',
       component: CalendarView
     },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: SettingsView
+    }
   ]
 })
 
