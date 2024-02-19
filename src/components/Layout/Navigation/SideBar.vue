c70d0f09aa31ddb3755d0836441e901abfc9c838
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-02-19 22:07:35 +0200

    Add Rotas & Shifts route

--- src/components/Layout/Navigation/SideBar.vue
+++ src/components/Layout/Navigation/SideBar.vue
@@ -120,7 +120,7 @@ watch(sidebarExpanded, () => {
             >
             <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
           </h3>
-          <ul class="mt-3">
+          <ul class="mt-10">
             <!-- Nav item -->
             <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0">
               <router-link
@@ -307,6 +307,92 @@ watch(sidebarExpanded, () => {
                   <span
                     class="justify-center text-[11px] text-center hidden pt-0 w-full no-underline active:no-underline focus:no-underline leading-3 mt-2 lg:flex text-white"
                     >Calendar</span
+                  >
+                </span>
+              </router-link>
+            </li>
+            <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0">
+              <router-link
+                class="relative flex flex-col items-center justify-center w-16 h-full lg:w-full"
+                to="/"
+                @click="sidebarExpanded ? $emit('close-sidebar') : null"
+              >
+                <span class="flex flex-col items-center justify-center">
+                  <svg
+                    class="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:cc="http://creativecommons.org/ns#"
+                    xmlns:dc="http://purl.org/dc/elements/1.1/"
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                  >
+                    <g transform="translate(0 -1028.4)">
+                      <path
+                        d="m5 1032.4c-1.1046 0-2 0.9-2 2v14c0 1.1 0.8954 2 2 2h6 2 6c1.105 0 2-0.9 2-2v-14c0-1.1-0.895-2-2-2h-6-2-6z"
+                        fill="#bdc3c7"
+                      />
+                      <path
+                        d="m5 3c-1.1046 0-2 0.8954-2 2v14c0 1.105 0.8954 2 2 2h6 2 6c1.105 0 2-0.895 2-2v-14c0-1.1046-0.895-2-2-2h-6-2-6z"
+                        fill="#ecf0f1"
+                        transform="translate(0 1028.4)"
+                      />
+                      <path
+                        d="m5 3c-1.1046 0-2 0.8954-2 2v3 1h18v-1-3c0-1.1046-0.895-2-2-2h-6-2-6z"
+                        fill="#e74c3c"
+                        transform="translate(0 1028.4)"
+                      />
+                      <path
+                        d="m7 5.5a1.5 1.5 0 1 1 -3 0 1.5 1.5 0 1 1 3 0z"
+                        fill="#c0392b"
+                        transform="translate(.5 1028.4)"
+                      />
+                      <path
+                        d="m6 1c-0.5523 0-1 0.4477-1 1v3c0 0.5523 0.4477 1 1 1s1-0.4477 1-1v-3c0-0.5523-0.4477-1-1-1z"
+                        fill="#bdc3c7"
+                        transform="translate(0 1028.4)"
+                      />
+                      <path
+                        d="m7 5.5a1.5 1.5 0 1 1 -3 0 1.5 1.5 0 1 1 3 0z"
+                        fill="#c0392b"
+                        transform="translate(12.5 1028.4)"
+                      />
+                      <g fill="#bdc3c7">
+                        <path
+                          d="m18 1029.4c-0.552 0-1 0.4-1 1v3c0 0.5 0.448 1 1 1s1-0.5 1-1v-3c0-0.6-0.448-1-1-1z"
+                        />
+                        <path
+                          d="m5 1039.4v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2z"
+                        />
+                        <path
+                          d="m5 1042.4v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2z"
+                        />
+                        <path
+                          d="m5 1045.4v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2zm3 0v2h2v-2h-2z"
+                        />
+                      </g>
+                      <path
+                        d="m40 18a6 6 0 1 1 -12 0 6 6 0 1 1 12 0z"
+                        fill="#34495e"
+                        transform="matrix(.82491 0 0 .82491 -9.5468 1032.1)"
+                      />
+                      <path
+                        d="m18.5 14c-0.276 0-0.5 0.224-0.5 0.5v0.5 3 0.5c0 0.276 0.224 0.5 0.5 0.5h0.5 2 0.5c0.276 0 0.5-0.224 0.5-0.5s-0.224-0.5-0.5-0.5h-0.5-2v-3-0.5c0-0.276-0.224-0.5-0.5-0.5z"
+                        fill="#bdc3c7"
+                        transform="translate(0 1028.4)"
+                      />
+                      <path
+                        d="m18.5 1041.4c-3.038 0-5.5 2.4-5.5 5.5 0 3 2.462 5.5 5.5 5.5s5.5-2.5 5.5-5.5c0-3.1-2.462-5.5-5.5-5.5zm0 1c2.485 0 4.5 2 4.5 4.5 0 2.4-2.015 4.5-4.5 4.5s-4.5-2.1-4.5-4.5c0-2.5 2.015-4.5 4.5-4.5z"
+                        fill="#7f8c8d"
+                      />
+                      <path
+                        d="m27 17.5c0 0.276-0.224 0.5-0.5 0.5s-0.5-0.224-0.5-0.5 0.224-0.5 0.5-0.5 0.5 0.224 0.5 0.5z"
+                        fill="#95a5a6"
+                        transform="translate(-8 1029.4)"
+                      />
+                    </g>
+                  </svg>
+                  <span
+                    class="justify-center text-[10px] text-center hidden pt-0 w-full no-underline active:no-underline focus:no-underline leading-3 mt-2 lg:flex text-white"
+                    >Rotas & Shifts</span
                   >
                 </span>
               </router-link>
